dcd7462148c81df514d70bdc8917d5d3ee66279d
Carl <carlvann16@gmail.com>
2025-06-19 01:45:19 +0000

    summary

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class DiscordClient(discord.Client):
 
     IGNORED_USERS = ['Animingle']
     ACCEPTABLE_BANGS = ['!an', '!am', '!animingle']
-    ACCEPTABLE_COMMANDS = ['recent', 'help']
+    ACCEPTABLE_COMMANDS = ['recent', 'help', 'all', 'summary']
 
     HEADERS = {
         'X-MAL-CLIENT-ID': os.getenv('CLIENT_ID')
@@ -34,18 +34,24 @@ class DiscordClient(discord.Client):
 
         error, verified = self._verify_msg(command)
 
-        #if not verified:
-        #    await message.channel.send(content=error, mention_author=True)
-        #    return
+        if not verified:
+            await message.channel.send(content=error, mention_author=True)
+            return
 
         response = self._process_msg(command)
-        pprint(response)
-        #await message.channel.send(response)
+        #pprint(response)
+        await message.channel.send(response)
 
     def get_recent_anime(self, user, amount):
+        output_string = f"Top {amount} recent anime:\n"
         req_url= f'https://api.myanimelist.net/v2/users/{user}/animelist?fields=list_status&limit={amount}&sort=anime_start_date'
         resp = requests.get(req_url, headers=self.HEADERS)
-        return resp.json()
+        resp = resp.json().get('data')
+
+        for anime in resp:
+            output_string += anime['node']['title'] + '\n'
+
+        return output_string
 
     def get_all_anime(self, user):
         req_url = f'https://api.myanimelist.net/v2/users/{user}/animelist?fields=list_status&limit=1000&sort=anime_title'
@@ -97,8 +103,9 @@ class DiscordClient(discord.Client):
             'other': other
         }
 
-        pprint(summary)
+        summary_result = f"Anime Watched: {summary['total_anime_watched']} \nCompleted: {completed} \nOngoing: {watching} \nDropped: {dropped} \nUpcomming: {upcomming} \nOther: {other}"
 
+        return summary_result
 
     def _verify_msg(self, command):
 
